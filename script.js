75e3416b546698b5c9523c41341bacaa4e95cb04
zkrem56 <zkremer702@gmail.com>
2022-10-04 13:44:52 -0500

    Got information from form putting it through form submission

--- script.js
+++ script.js
@@ -2,6 +2,21 @@
 
 window.addEventListener("load", function() {
 
+    let form = document.getElementById("launchForm");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let formData = document.querySelectorAll("input[type=text]");
+        let pilotName = formData[0].value;
+        let copilotName = formData[1].value;
+        let fuellevel = formData[2].value;
+        let cargoMass = formData[3].value;
+
+        formSubmission(document, formData, pilotName, copilotName, fuellevel, cargoMass);
+
+
+    });
+
     let listedPlanets;
     // Set listedPlanetsResponse equal to the value returned by calling myFetch()
     let listedPlanetsResponse;
